fb6e606c0b26043820197a1607e6f3b1f88dff20
Евгений <ivanychcos@gmail.com>
2022-08-28 10:36:50 +0300

    Add example 013

---
+++ Example013/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int x = int.Parse(Console.ReadLine());
+if (x >= 100)
+{
+    while (x >= 1000)
+    x = x / 10;
+    
+    x = x % 10;
+    Console.WriteLine(x);
+}
+else
+{
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+}
