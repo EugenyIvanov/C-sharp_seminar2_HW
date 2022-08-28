18762d9d58c88f871d59f6bfcb3146f7277f94f0
Евгений <ivanychcos@gmail.com>
2022-08-28 11:21:16 +0300

    Add example without number

---
+++ Example_withoutNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine());
+int min = a;
+ if (b < min) min = b;
+ if (c < min) min = c;
+ Console.WriteLine("min = " + min);
+ int sum = (a + b + c) / 3;
+ Console.WriteLine("sum = " + sum);
+ int m = min / 100;
+ int n = sum % 10;
+ if (n > m)
+ {
+    Console.Write("Да");
+ }
+ if (n == m)
+ {
+    Console.Write("Цифры равны");
+ }
+ if (n < m)
+ {
+    Console.Write("Нет");
+ }
